5bd114e1c7c2793ff094ff098e40efa2bf4168e0
Nami2012 <namithasadeeshbabu@gmail.com>
2019-09-02 16:20:54 +0530

    Disk Scheduling

--- disk_cscan.c
+++ disk_cscan.c
@@ -2,71 +2,74 @@
 #include<stdlib.h>
 #include<time.h>
 
+
+
+// configure gnuplot
 void plot(int *s, int head, int n, char str[])
 	{
-    	char * commandsForGnuplot[] = {"set style data line", "plot [0:250][0:10] 'data.temp'"};
+    	char * commandsForGnuplot[] = {"set xlabel 'BURST TIME' ", "set ylabel 'Time'", "set style data line", "plot [0:250][0:10] 'data.temp' , '' with labels "};
     	FILE * temp = fopen("data.temp", "w");
     	FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
     	fprintf(temp, "%d 0 \n", head);
     	for (int i=0; i < n; i++)
-        	fprintf(temp, "%d %d \n", s[i], i+1);
+        	fprintf(temp, "%d %d %d \n", s[i], i+1,s[i]);
     
     	fprintf(gnuplotPipe, "set title \"%s\" \n", str);
 
-    	for (int i=0; i < 2; i++)
+    	for (int i=0; i < 4; i++)
         	fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]);
-	}
-
+	} 
 
 //calculate total head movements 
 int total (int head, int n, int *seek_pos , char ch)
 	{	int *s = (int *)malloc(sizeof(int *)*n);
 		int j=0,real_head=head;
 	 	int total = 0; int pos ,flag = 0;
-	    for(int i=0;i<n;i++)
+		
+	   //find pivot position 
+	   for(int i=0;i<n;i++)
 	    {if(head<seek_pos[i])
 	    	{ pos = i; flag = 1;break;}
 	    }
 	    if(flag ==0)
 	    pos = n - 1;
 	    
-	    printf(" ch = %c",ch);
+	    //printf(" ch = %c",ch);
+	    
 	    printf(" \n Scheduled queue is \n");
 	    if(ch == 'l')
 	    	{ for(int i=pos-1;i>=0;i--)
-	    		{ printf(" %d",seek_pos[i]); 
-	    		  s[j++] = seek_pos[i];
+	    		{ s[j++]=seek_pos[i];
+			  printf(" %d ",seek_pos[i]); 
 	    		  total += abs(head - seek_pos[i]);
 	    		  head = seek_pos[i]; }
-	    		  s[j++] = 0;
-	    		  s[j++] = 250;
-	     	  for(int i=n;i>pos;i--)
-	    		{ s[j++] = seek_pos[i];
-	    		  printf(" %d",seek_pos[i]); 
+	    	  for(int i=n-1;i>=pos;i--)
+	    		{ s[j++]=seek_pos[i];
+			  printf(" %d",seek_pos[i]); 
 	    		  total += abs(head - seek_pos[i]);
 	    		  head = seek_pos[i];
 	    		   }
 	        }
 	    
-	    if(ch == 'r')
+	    else
 	    	{ for(int i=pos;i<n;i++)
-	    		{ s[j++] = seek_pos[i];
-	    		  printf(" %d",seek_pos[i]); 
+	    		{ s[j++]=seek_pos[i];
+			  printf(" %d",seek_pos[i]); 
 	    		  total += abs(head - seek_pos[i]);
 	    		  head = seek_pos[i]; }
 	    		  s[j++] = 250;
 	    		  s[j++] = 0;
-	    	  for(int i=0;i>=pos-1;i++)
-	    		{ s[j++] = seek_pos[i];
-	    		  printf(" %d",seek_pos[i]); 
+	    	  for(int i=0;i<pos;i++)
+	    		{ s[j++]=seek_pos[i];
+			  printf(" %d",seek_pos[i]); 
 	    		  total += abs(head - seek_pos[i]);
 	    		  head = seek_pos[i]; }
 	     	 }
 	     	 
 	     	 
-			//gnuplot
-			char str[] = "SCAN";
-    		plot(s,real_head,n,str);
+		  //gnuplot
+	          char str[] = "SCAN";
+    		  plot(s,real_head,n,str);
 	     return total;
 	     }
 	
@@ -88,7 +91,7 @@ void sort(int *seek_pos, int n)
 
 	 
 void main()
-{
+{	printf("CSCAN");
 	srand(time(0));
 	
 	//generate number of requests, seek position, current head position
@@ -98,6 +101,7 @@ void main()
 	int head = rand()%250;
 	for(int i=0; i<n; i++)
 		 seek_pos[i] = rand()%250;  
+	
 	//print schedule
 	
 	printf("head position = %d \n",head);
@@ -106,7 +110,12 @@ void main()
 		printf(" %d ",seek_pos[i]); 
 	sort(seek_pos,n);
 	char option[2] = {'l','r'};
-	printf("\n Total head movements = %d \n", total(head,n,seek_pos,'l'));
+	int total_movement = total(head,n,seek_pos,(char)option[rand()%2 + 1]);
+	printf("\n Total head movements = %d \n",total_movement );	
+	//seek time calculation
+        int move_time = rand()%10 + 1;
+	float avg_seek_time = (float)(move_time * total_movement)/n;        
+	printf("\n Move time = %d ms\n seek time = %0.3f \n",move_time,avg_seek_time);
 	                                                      
 	 
  }
