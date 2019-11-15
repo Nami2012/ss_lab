f238fa110a380d8abc042d4e419033cbeccc52c6
Sreelekshmi41159 <sreelekshmisreeju@gmail.com>
2019-11-15 23:35:02 +0530

    jsfdgj

--- assembler/firstpass.cpp
+++ assembler/firstpass.cpp
@@ -5,6 +5,7 @@
 #include<algorithm>
 #include<map> 
 #include<bits/stdc++.h>
+#include<cmath>
 using namespace std;
 string int_to_hex(int n)
     {   string hex_value;
@@ -27,92 +28,86 @@ string int_to_hex(int n)
         return hex_value; 
          }
 
+int hex_to_int(int n)
+		{ 	int i=0,temp,hex=0;
+			while(n!=0)
+			{ temp = n%10;
+			  hex += temp*pow(16,i++);
+			  n = n/10;
+			}
+		    return hex;
+		  }
 
-int verify_opcode(string opcode)
+int verify_opcode(string opcode,int &LOCCTR,string operand)
     {  
        fstream file;
        string word;
        file.open("opcode.txt");
        while(file>>word)
-        {  if(word.compare(opcode)==0)
-              {return 1;}}               
+        	{
+        	if(word.compare(opcode)==0)
+            	  {if(opcode[0]=='+') 
+            	  		LOCCTR +=4;
+            	   else if(opcode[opcode.size()-1] =='R')
+            	   		LOCCTR +=2;
+            	   else
+            	   		LOCCTR += 3;return 1;}
+            	  }               
        if((opcode.compare("START")==0) || (opcode.compare("END")==0))
-       			return 2;
-        if(opcode.compare("WORD")==0)
-				return 3;
-        if(opcode.compare("RESW")==0)
-				return 4;
-	   if(opcode.compare("RESB")==0)
-				return 5;
-	   if(opcode.compare("BYTE")==0)
-				return 6;
-       return -1;
+       					{LOCCTR +=0;return 1;}
+       		else if(opcode.compare("WORD")==0)
+						{LOCCTR += 3;return 1;}
+        	else if(opcode.compare("RESW")==0)
+						{LOCCTR += 3*stoi(operand);return 1;}
+	   		else if(opcode.compare("RESB")==0)
+						{LOCCTR += stoi(operand);return 1;}
+	   		else if(opcode.compare("BYTE")==0)
+						{{ if (operand[0] == 'X') 
+								LOCCTR++;
+							else
+						  		LOCCTR += operand.size()-3;
+							}
+						return 1;}
+       else
+       		return -1;
     }
     
-int update(int mneumonic,int &LOCCTR,string operand)
-	{
-		if(mneumonic == 1)
-			LOCCTR += 3;
-		if (mneumonic ==3)
-			LOCCTR +=3;
-	    if (mneumonic == 4)
-			LOCCTR += 3*stoi(operand);
-		 if (mneumonic == 5)
-			LOCCTR += stoi(operand);
-		if (mneumonic == 6)
-			{ if (operand[0] == 'X') 
-					LOCCTR++;
-			  else
-			  	LOCCTR += operand.size()-3;
-			};}
 
 int main()
-{   map<string,string > SYMTAB; 
-    int LOCCTR=0000;
-	FILE *fptr;
-	ofstream fout,fim;
-	fim.open("intermediate.txt");
-	int mneumonic;
-    string opcode,LABEL,operand;
-	fptr = fopen("input.txt", "r"); 
+	{   map<string,string > SYMTAB; 
+    	int LOCCTR=0000;
+		FILE *fptr;
+		ifstream fin;
+		ofstream fout,fim;
+		fim.open("intermediate.txt");
+		int mneumonic;
+    	string opcode,LABEL,operand;
+		fin.open("sic.txt");
 	
-	char line[50];
-	 while(fgets(line,50,fptr)!= NULL) 
-	  {    int j = 0 , i=0;
-        opcode.clear();
-        LABEL.clear();
-        operand.clear();
-	    while(line[i]!='-' && line[i]!=' ')
-           { LABEL.push_back(line[i++]);
-           }
-        while(line[++i] == ' ');
-		while(line[i]!=' ')
-           { opcode.push_back(line[i++]);
-           }
-        while(line[++i] == ' ');
-        while(line[i]!='\n')
-           { operand.push_back(line[i++]);
-           }
-       mneumonic = verify_opcode(opcode);
-       string hexLOCCTR = int_to_hex(LOCCTR);
-      
-       if(mneumonic!=-1)
-       		update(mneumonic,LOCCTR,operand);
-       if(LABEL.size() != 0)
-       		SYMTAB.insert({LABEL,hexLOCCTR});
-   	   if(LABEL.size()==0)
-   	   fim<<hexLOCCTR<<" "<<"-"<<" "<<opcode<<" "<<operand;
-   	   else
-   	   fim<<hexLOCCTR<<" "<<LABEL<<" "<<opcode<<" "<<operand;
-   	   fim<<endl;
-        }
-       fout.open("symtab.txt"); 
+		fin>>LABEL>>opcode>>operand;
+		LOCCTR=hex_to_int(stoi(operand));
+		
+		while(opcode.compare("END")!=0)
+		{
+			     mneumonic = verify_opcode(opcode,LOCCTR,operand);
+    	         string hexLOCCTR = int_to_hex(LOCCTR);
+    			 if((LABEL.size() != 0)&& (LABEL.compare("-")!=0))
+    	   	     	  SYMTAB.insert({LABEL,hexLOCCTR});
+				 if(LABEL.size()==0)
+   		   		 	  fim<<hexLOCCTR<<" "<<"-"<<" "<<opcode<<" "<<operand;
+   		     	 else
+   		   			  fim<<hexLOCCTR<<" "<<LABEL<<" "<<opcode<<" "<<operand;
+   		     	 fim<<endl;
+    	         fin>>LABEL>>opcode>>operand;
+	   	}
+	
+        fout.open("symtab.txt"); 
 	 
-       for(map<string, string >::iterator i=SYMTAB.begin(); i!=SYMTAB.end(); i++)
-       {
-        fout<<i->first<<" "<<i->second;}
-        fclose(fptr);
+        for(map<string, string >::iterator i=SYMTAB.begin(); i!=SYMTAB.end(); i++)
+         {
+           fout<<i->first<<" "<<i->second<<endl;}
+           fin.close();
     	fout.close();
         return 0;
 
-} 
+	} 
--- assembler/twopass.cpp
+++ assembler/twopass.cpp
@@ -7,11 +7,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main()
 {  ifstream fin;
+   ofstream fout;
    string hexLOCCTR,LABEL,opcode,operand;
    fin.open("intermediate.txt");
+   fout.open("objectcode.txt");
    fin>>hexLOCCTR>>LABEL>>opcode>>operand;
+   
+   fout<<"h^"<<LABEL<<"^"<<hexLOCCTR<<"^";
+   
+   
    cout<<hexLOCCTR<<LABEL<<opcode<<operand;
    
  }
